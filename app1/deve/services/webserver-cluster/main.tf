fa2b840ec5de7b5c0c40430a19465490594f0905
Sid <navyasiddhu@gmail.com>
2021-03-28 18:30:38 +0800

    added modules

---
+++ app1/deve/services/webserver-cluster/main.tf
@@ -0,0 +1,25 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.0"
+    }
+  }
+}
+
+# Configure the AWS Provider
+provider "aws" {
+  region = "us-east-2" 
+  }
+  
+module "webserver_cluster" {
+  source = "../../../modules/services/webserver-cluster"
+
+  cluster_name            = "webservers-stage"
+  instance_type           = "t2.micro"
+  webport                 = 8080
+  sshport                 = 22
+  public_cidr             = "0.0.0.0/0"
+  tags                    = {"owner": "terraform", "env": "deve"}
+  region = "us-east-2" 
+}
---
+++ app1/modules/services/webserver-cluster/variables.tf
@@ -0,0 +1,28 @@
+variable "webport" {
+default = 8080
+type = number
+}
+
+variable "public_cidr" {
+  type = any
+}
+
+variable "tags" {
+  type = map(string)
+}
+
+variable "sshport" {
+  type = number
+}
+
+variable "instance_type" {
+  type = string
+}
+
+variable "cluster_name" {
+  type = string
+}
+
+variable "region" {
+  type = string
+}
